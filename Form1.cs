159c8ca26cbb36088946d26a8d94cac375b2b5f6
Benyaphaaa <61322689+Benyaphaaa@users.noreply.github.com>
2020-11-25 22:23:52 +0700

    Save
    Save

---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a, b, c;
+            a = int.Parse(textBox1.Text);
+            b = int.Parse(textBox2.Text);
+            c = gcd(a, b);
+            textBox3.Text = c.ToString();
+
+        }
+        static int gcd(int n1, int n2)
+        {
+            if (n2 == 0)
+            {
+                return n1;
+            }
+            else
+            {
+                
+                return gcd(n2, n1 % n2);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            {
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox1.Focus();
+            }
+        }
+    }
+}
